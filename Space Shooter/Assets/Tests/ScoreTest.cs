7c96450c71f9cbd19c8bc2a0eb5a86712033e4fd
Viditv68 <viditrawatv68@gmail.com>
2021-02-20 16:39:53 +0530

    Testing

---
+++ Space Shooter/Assets/Tests/ScoreTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ScoreTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ScoreTests()
+        {
+            var uiManager = new GameObject().AddComponent<UIManager>();
+
+
+            uiManager.updateScore();
+
+            int expect = 10;
+
+            Assert.AreEqual(uiManager.score, expect);
+
+
+
+        }
+
+        
+    }
+}
